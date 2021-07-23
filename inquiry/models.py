e613e618457f56808d68bade64c0af51da7a194e
tim <timjonez@protonmail.com>
2021-07-23 13:40:19 -0500

    [inquiry/models] add method to create from request

--- inquiry/models.py
+++ inquiry/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 import pytz
+from ipware import get_client_ip
 from .utils import HTTP_STATUS_CODES
 from django.conf import settings
 
@@ -11,10 +11,27 @@ class Request(models.Model):
     method = models.CharField(default='GET', max_length=7)
     path = models.CharField(max_length=255)
     time = models.DateTimeField(default=timezone.now, db_index=True)
-    timezone = models.CharField(choices=[(tz, tz) for tz in pytz.all_timezones])
+    timezone = models.CharField(max_length=100, choices=[(tz, tz) for tz in pytz.all_timezones])
     ip = models.GenericIPAddressField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
     referer = models.URLField(max_length=255, blank=True, null=True)
     user_agent = models.CharField(max_length=255, blank=True, null=True)
     language = models.CharField(max_length=255, blank=True, null=True)
     is_secure = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.time} {self.method} {self.path} {self.response}"
+
+    def create_from_request(self, request, response):
+        self.method = request.method
+        self.path = request.path
+        self.is_secure = request.is_secure()
+
+        self.ip, _ = get_client_ip(request) if not None else '0.0.0.0'
+        self.referer = request.META.get('HTTP_REFERER', '')[:255]
+        self.user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
+        self.language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')[:255]
+        if request.user:
+            if request.user.is_authenticated:
+                self.user = request.user
+        self.save()
